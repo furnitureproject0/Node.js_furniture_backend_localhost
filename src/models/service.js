4832ab1c7016596322946f264be5aee781934bb0
Ibrahim Adel <ibrahimadel.dev@gmail.com>
2026-02-17 07:10:45 +0200

    edit service model

--- src/models/service.js
+++ src/models/service.js
@@ -15,6 +15,20 @@ const Service = sequelize.define('Service', {
     description: {
         type: DataTypes.TEXT,
         allowNull: true
+    },
+    discount: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: true,
+        defaultValue: 0
+    },
+    pricing_type: {
+        type: DataTypes.ENUM('per_hour', 'per_square_meter', 'per_cubic_meter', 'per_quantity', 'per_room', 'flat_rate', 'custom'),
+        allowNull: false
+    },
+    is_active: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
     }
 }, {
     timestamps: true,
