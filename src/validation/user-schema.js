538fed3b673b4560d0576ecf095a93e7a0086926
Ibrahim Adel <ibrahimadel.dev@gmail.com>
2026-02-17 23:26:20 +0200

    edit user-schema

--- src/validation/user-schema.js
+++ src/validation/user-schema.js
@@ -1,5 +1,6 @@
 import Joi from "joi";
 import { name, birthdate, password, email, phones } from "./global-schemas.js";
+import { locationSchema } from "./location-schema.js";
 
 export const userBaseSchema = Joi.object({
     email,
@@ -7,4 +8,19 @@ export const userBaseSchema = Joi.object({
     name,
     password,
     birthdate,
+    role: Joi.string()
+        .valid(
+        'super_admin',
+        'site_admin',
+        'company_admin',
+        'company_secretary',
+        'driver',
+        'worker',
+        'client'
+        )
+        .optional(),
+    company_id: Joi.number()
+        .integer()
+        .optional(),
+    locationSchema,
 })
