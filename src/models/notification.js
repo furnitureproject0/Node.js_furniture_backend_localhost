a8a38cf38f4c23d1fc954724e1e2e8269353baec
Ibrahim Adel <ibrahimadel.dev@gmail.com>
2026-02-19 21:48:17 +0200

    edit nonification model

--- src/models/notification.js
+++ src/models/notification.js
@@ -2,91 +2,74 @@ import { DataTypes } from 'sequelize';
 import sequelize from '../config/database.js';
 import User from './user.js';
 
+
 const Notification = sequelize.define('Notification', {
+
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true
     },
-    user_id: {
+
+    actor_id: {
         type: DataTypes.INTEGER,
-        allowNull: false,
+        allowNull: true,
         references: {
             model: User,
             key: 'id'
         }
     },
+
     title: {
         type: DataTypes.STRING,
         allowNull: false
     },
+
     message: {
         type: DataTypes.TEXT,
         allowNull: false
     },
+
     type: {
         type: DataTypes.ENUM(
-            'welcome',
-            'email_verified',
-            'order',
-            'offer',
-            'chat',
-            'employment',
-            'system',
+            'welcome', //
+            'email_verified', //
+            'order', //
+            'offer', //
+            'chat', //
+            'employment', //
             // add more types as needed
-            'admin_alerts',
-            'company_updates',
-            'security_alerts',
-            'promotions',
-            'reminders',
-            'feedback_requests',
-            'order_status',
             'internal_company',
             'external_company',
             'mother_company',
-            'account_deleted',
-            'update'
+            //
+            'system',
+            'account',
+            'company',
+            'security'
         ),
         allowNull: false,
         defaultValue: 'system'
     },
+
+    entity_type: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+
+    entity_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+
     payload: {
         type: DataTypes.JSON,
         allowNull: true
-    },
-    is_read: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: false
-    }, 
-    show: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: true
     }
+
 }, {
     tableName: 'notifications',
-    timestamps: true,
-    indexes: [
-        {
-            fields: ['user_id']
-        },
-        {
-            fields: ['createdAt']
-        }
-    ]
+    timestamps: true
 });
-
-// Automatically delete notifications older than 30 days
-// Notification.deleteOld = async () => {
-//     const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
-//     await Notification.destroy({
-//         where: {
-//             created_at: {
-//                 [DataTypes.Op.lt]: thirtyDaysAgo
-//             }
-//         }
-//     });
-// };
 
 export default Notification;
