785a6cbe8f323f84773fc73bfce3e0455996c674
Ibrahim Adel <ibrahimadel.dev@gmail.com>
2026-02-22 07:57:57 +0200

    add update service to vehicle controller

--- src/controllers/vehicle-controller.js
+++ src/controllers/vehicle-controller.js
@@ -2,7 +2,7 @@
 
 import asyncHandler from 'express-async-handler';
 import AppError from '../utils/AppError.js';
-import { addVehicle } from '../services/vehicle/index.js';
+import { addVehicle, updateVehicle } from '../services/vehicle/index.js';
 
 export const createVehicle = asyncHandler(async (req, res) => {
     try {
@@ -16,5 +16,21 @@ export const createVehicle = asyncHandler(async (req, res) => {
         });
     } catch (error) {
         throw new AppError(error.message || 'Failed to create vehicle', error.statusCode || 500);
+    }
+});
+
+export const editVehicle = asyncHandler(async (req, res) => {
+    const vehicleId = req.params.id;
+    try {
+        const updatedVehicle = await updateVehicle(vehicleId, req.body);
+        res.status(200).json({
+            success: true,
+            message: 'Vehicle updated successfully',
+            data: {
+                vehicle: updatedVehicle
+            }
+        });
+    } catch (error) {
+        throw new AppError(error.message || 'Failed to update vehicle', error.statusCode || 500);
     }
 });
--- src/services/vehicle/index.js
+++ src/services/vehicle/index.js
@@ -1,1 +1,2 @@
 export * from './vehicle-add-service.js';
+export * from './vehicle-update-service.js';
