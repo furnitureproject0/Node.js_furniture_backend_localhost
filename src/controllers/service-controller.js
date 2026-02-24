0982bffe976c6915cfb597306a72030dbfdc06f5
Ibrahim Adel <ibrahimadel.dev@gmail.com>
2026-02-24 10:10:42 +0200

    make service-controller

---
+++ src/controllers/service-controller.js
@@ -0,0 +1,36 @@
+import asyncHandler from 'express-async-handler';
+import AppError from '../utils/AppError.js';
+import { getAllServices, createService, updateService } from '../services/service/index.js'
+
+export const getServices = asyncHandler(async (req, res) => {
+    const { search = '', page = 1, limit = 10, ...filters } = req.query;
+    
+    const pagination = {
+        page: parseInt(page) || 1,
+        limit: parseInt(limit) || 10
+    };
+    const result = await getAllServices(filters, search, pagination, req.user);
+    res.status(200).json({
+        success: true,
+        message: 'Services retrieved successfully',
+        data: result
+    });
+});
+
+export const createNewService = asyncHandler(async (req, res) => {
+    const addition = await createService(req.body);
+    res.status(201).json({
+        success: true,
+        message: 'Service created successfully',
+        data: addition
+    });
+});
+
+export const updateServiceById = asyncHandler(async (req, res) => {
+    const addition = await updateService(req.params.id, req.body);
+    res.status(200).json({
+        success: true,
+        message: 'Service updated successfully',
+        data: addition
+    });
+});
