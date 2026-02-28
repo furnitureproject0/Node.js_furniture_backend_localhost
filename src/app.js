97aa26d4a136ebd98000eb993f4213481d9e3871
Ibrahim Adel <ibrahimadel.dev@gmail.com>
2026-03-01 01:49:47 +0200

    cors

--- src/app.js
+++ src/app.js
@@ -46,10 +46,11 @@ import notification from './routes/notification-routes.js';
 const app = express();
 
 // Middleware
-app.use(cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true
-}));
+// app.use(cors({
+//     origin: process.env.CLIENT_URL,
+//     credentials: true
+// }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
