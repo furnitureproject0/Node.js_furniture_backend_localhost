454d71794b7da6c1c04f08bc26b66b242b423566
Ibrahim Adel <ibrahimadel.dev@gmail.com>
2026-03-02 00:30:57 +0200

    get orders

--- src/routes/order-routes.js
+++ src/routes/order-routes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.use(protect);
 
-router.get('/', authorize('super_admin', 'site_admin', 'company_admin', 'company_secretary'), getOrders);
+router.get('/', getOrders);
 router.post('/admin-create-order', authorize('super_admin', 'site_admin', 'company_admin', 'company_secretary'), validate(createOrderForClientSchema), adminCreateOrderForClient);
 router.patch('/admin-update-order/:id', authorize('super_admin', 'site_admin', 'company_admin', 'company_secretary'), validate(updateOrderForClientSchema), adminUpdateOrderForClient);
 router.patch('/:id/cancel', authorize('super_admin', 'site_admin', 'company_admin', 'company_secretary'), cancelOrderByAdmin);
